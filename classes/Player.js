080669be19857675670afa31c7286a186b8d9571
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-03-17 22:34:44 +0100

    add show collision area method

--- classes/Player.js
+++ classes/Player.js
@@ -47,6 +47,11 @@ class Player extends Sprite {
         this.velocity.y += this.gravity;
         this.position.y += this.velocity.y;
     }
+
+    showCollisionArea(block) {
+        drawingSurface.fillStyle = "blue";
+        drawingSurface.fillRect(block.position.x, block.position.y, block.width, block.height)
+    }
     
     checkCollisionsX() {
         for (let i = 0; i < this.collisionBlocks.length; i++) {
@@ -58,16 +63,14 @@ class Player extends Sprite {
                 && this.hitbox.position.x <= block.position.x + block.width
                 && this.hitbox.position.x + this.hitbox.width >= block.position.x) {
                 if(this.velocity.x < 0) {
-                    drawingSurface.fillStyle = "blue";
-                    drawingSurface.fillRect(block.position.x, block.position.y, block.width, block.height)
+                    this.showCollisionArea(block);
                     const offset = this.hitbox.position.x - this.position.x
                     this.position.x = block.position.x + block.width - offset + 0.1;
                     // this.position.x = block.position.x + block.width + 0.1;
                     break;
                 }
                 if(this.velocity.x > 0) {
-                    drawingSurface.fillStyle = "blue";
-                    drawingSurface.fillRect(block.position.x, block.position.y, block.width, block.height)
+                    this.showCollisionArea(block);
                     const offset = this.hitbox.position.x - this.position.x + this.hitbox.width;
                     this.position.x = block.position.x - offset - 0.1;
                     // this.position.x = block.position.x - this.width - 0.1;
@@ -88,8 +91,7 @@ class Player extends Sprite {
                 && this.hitbox.position.x + this.hitbox.width >= block.position.x) {
                     if(this.velocity.y < 0) {
                         this.velocity.y = 0;
-                        drawingSurface.fillStyle = "blue";
-                        drawingSurface.fillRect(block.position.x, block.position.y, block.width, block.height)
+                        this.showCollisionArea(block);
                         const offset = this.hitbox.position.y - this.position.y
                         this.position.y = block.position.y + block.height - offset + 0.1;
                         // this.position.y = block.position.y + block.height + 0.1;
@@ -97,8 +99,7 @@ class Player extends Sprite {
                     }
                     if(this.velocity.y > 0) {
                         this.velocity.y = 0;
-                        drawingSurface.fillStyle = "blue";
-                        drawingSurface.fillRect(block.position.x, block.position.y, block.width, block.height)
+                        this.showCollisionArea(block);
                         const offset = this.hitbox.position.y - this.position.y + this.hitbox.height;
                         this.position.y = block.position.y - offset - 0.1;
                         // this.position.y = block.position.y - this.height - 0.1;
